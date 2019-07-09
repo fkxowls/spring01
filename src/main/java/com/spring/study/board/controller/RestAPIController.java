56bbc073f27a9d6408dc3002717ee9b20ab79225
jnty0 <jnty0@192.168.0.6>
2019-07-09 23:21:15 +0900

    과제 완료후에 restAPI 컨트롤러 분리할거임

---
+++ src/main/java/com/spring/study/board/controller/RestAPIController.java
@@ -0,0 +1,9 @@
+package com.spring.study.board.controller;
+
+/************************************
+	restAPI용 컨트롤러를 따로 분리해보자
+	
+************************************/
+public class RestAPIController {
+
+}
